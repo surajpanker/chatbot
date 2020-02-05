c2e586dce2df8b1d25dd73c39b2290bb568f8083
suraj82 <suraj82@bitbucket.org>
2020-02-05 18:11:09 +0530

    change

--- config/keys.js
+++ config/keys.js
@@ -1,11 +1,9 @@
-  if(proces.env.NODE_ENV==='production')
-    {
-        
-       module.export = require('./prod');
-        
-    }
-else{
-    module.export =require('./dev')
-    
-    
-}
+
+
+if (process.env.NODE_ENV === 'production') {
+    // we are in production - return the prod set of keys
+    module.exports = require('./prod');
+} else {
+    // we are in development - return the dev keys!!!
+    module.exports = require('./dev');
+} 
--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 //coding for backend app
 const express= require('express');
-const bodyParser = require('body-Parser');//for rotes
+const bodyParser = require('body-parser');//for 
 const app= express();
 
 app.use(bodyParser.json());
 
 
 require('./routes/dialogFlowRoutes')(app);
-
-
-
-
 
 const PORT =process.env.PORT||5000;
 app.listen(PORT);
