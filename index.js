61bd046c689fccca0ca707e8e27da37cece59b3b
suraj82 <suraj82@bitbucket.org>
2020-02-06 12:42:35 +0530

    update

--- index.js
+++ index.js
@@ -9,9 +9,5 @@ app.use(bodyParser.json());
 require('./routes/dialogFlowRoutes')(app);
 
 const PORT =process.env.PORT||5000;
-//app.listen(PORT);
-//change
-app.listen(port, () => {
-	console.log(`🌏 Server is running at http://localhost:${port}`)
-});
-// till here
+app.listen(PORT);
+
